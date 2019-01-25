774587b5e1f325445c97b12db6286b4e6ce336d6
Student <example@example.com>
2019-01-25 12:14:25 -0800

    remove if statement for 5's

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,4 @@
-// function beep(nums) {
-//   console.log(nums);
-//   var result = [];
-//
-//
-//    for (var i = 0; i < nums.length; i++) {
-//     if (nums[i].includes("5")) {
-//       result.push("0", "beep", "boop", "I'm sorry Dave. I'm afraid I can't do that.", "4", "5");
-//       break;
-//     } else if (nums[i] === "3"){
-//       result.push("I'm sorry Dave. I'm afraid I can't do that.")
-//
-//     } else if (nums[i] === "2"){
-//       result.push("boop")
-//
-//     } else if (nums[i] === "1"){
-//       result.push("beep")
-//
-//     }
-//   }
-//
-//  return result;
-// };
+
 
 function beep(nums) {
   var range = [];
