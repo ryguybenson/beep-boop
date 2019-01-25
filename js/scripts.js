05f416bc5f3b16e6586044bbb1d6eb585064d770
Student <example@example.com>
2019-01-25 09:46:36 -0800

    make nums not parseInt to test that if statement is working

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
 
+
+
 $(document).ready(function() {
-  $("formOne").submit(function(event) {
-    var input = parseInt($("#input").val());
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+
+    var nums = $("#number").val();
+    console.log(nums);
     var result = [];
+
+
+
+    for (var i = 0; i < nums.length; i++) {
+      if (nums[i] === "a") {
+        result.push("beep");
+      } else {
+        result.push(nums[i]);
+      }
+    }
+console.log(result);
+
+
+
+
+
+
   });
 });
