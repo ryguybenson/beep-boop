ee10754282cca53d8b32ab13774792543c4965c2
Student <example@example.com>
2019-01-25 09:56:11 -0800

    seperate logic in jscripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,23 @@
+function beep(nums) {
+console.log(nums);
+    var result = [];
 
+    for (var i = 0; i < nums.length; i++) {
+      if (nums[i] === "5") {
+        result.push("beep");
+      } else {
 
+      }
+    }
+
+ return result;
+};
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-
     var nums = $("#number").val();
-    console.log(nums);
-    var result = [];
-
-
-
-    for (var i = 0; i < nums.length; i++) {
-      if (nums[i] === "a") {
-        result.push("beep");
-      } else {
-        result.push(nums[i]);
-      }
-    }
-console.log(result);
-
-
-
-
+    var final = beep(nums);
 
 
   });
