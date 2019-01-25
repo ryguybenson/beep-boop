53256421d4955b10744ddef410230803562d491b
Student <example@example.com>
2019-01-25 10:53:32 -0800

    return all numbers leading up to inputted number

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,41 @@
+// function beep(nums) {
+//   console.log(nums);
+//   var result = [];
+//
+//
+//    for (var i = 0; i < nums.length; i++) {
+//     if (nums[i].includes("5")) {
+//       result.push("0", "beep", "boop", "I'm sorry Dave. I'm afraid I can't do that.", "4", "5");
+//       break;
+//     } else if (nums[i] === "3"){
+//       result.push("I'm sorry Dave. I'm afraid I can't do that.")
+//
+//     } else if (nums[i] === "2"){
+//       result.push("boop")
+//
+//     } else if (nums[i] === "1"){
+//       result.push("beep")
+//
+//     }
+//   }
+//
+//  return result;
+// };
+
 function beep(nums) {
-  console.log(nums);
-  var result = [];
-
-  for (var i = 0; i < nums.length; i++) {
-    if (nums[i] === "5") {
-      result.push("beep");
-      break;
-    } else {
-
-    }
+  var range = [];
+  for (var i = 0; i < nums+1; i++) {
+    range.push(i);
+    console.log(i);
   }
-
- return result;
-};
+  return range;
+}
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    var nums = $("#number").val();
+    var nums = parseInt($("#number").val());
+    console.log(nums);
     var final = beep(nums);
     console.log(final);
 
