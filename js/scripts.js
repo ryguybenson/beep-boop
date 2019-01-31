d8e2e2cdb621d3d855a3ec54617504aee05d2a59
Riley Benson <ryguybenson@yahoo.com>
2019-01-31 10:41:48 -0800

    add .includes to make aplication work properly

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@
     var range = [];
 
     for (var i = 0; i < nums + 1; i++) {
-      if (i.toString().split("") == "1") {
+      if (i.toString().split("").includes("1")) {
         range.push(" Beep ")
 
-      } else if (i.toString().split("") == "2") {
+      } else if (i.toString().split("").includes("2")) {
         range.push(" Boop ")
 
-      } else if (i.toString().split("") == "3") {
+      } else if (i.toString().split("").includes("3")) {
         range.push(" I'm sorry Dave. I'm afraid I can't do that. ")
 
       } else {
